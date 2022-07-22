a728b1520c940262b369f0aa08be456939b1404e
simon <simoncabrera.sc@gmail.com>
2022-07-21 22:49:50 -0300

    desafio2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+// UpperCamelCase
+
+const NavBar = () => {
+  return(
+    <nav>
+      <ul>
+        <li><button>Inicio</button></li>
+        <li><button>Productos</button></li>
+        <li><button>Mis compras</button></li>
+        <li><button>Contacto</button></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
